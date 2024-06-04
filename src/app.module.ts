cf36ed3583723d0b2a826302b6ab073df1872875
missShevel <o.v.shevel@gmail.com>
2024-06-04 22:44:03 +0300

    database with typeorm connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cat } from './cats/cats.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'cats_db',
+      entities: [Cat],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
--- src/cats/cats.entity.ts
+++ src/cats/cats.entity.ts
@@ -1,8 +1,31 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+
 export interface Cat {
-    id: string;
-    name: string;
-    color: string;
-    birthDate: Date;
-    createdAt: Date;
-    updatedAt: Date;
+  id: string;
+  name: string;
+  color: string;
+  birthDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+@Entity()
+export class Cat {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  color: string;
+
+  @Column()
+  birthDate: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
