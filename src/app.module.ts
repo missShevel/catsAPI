951296b0026c558a8a27d9da0fab60f4cd71e71d
missShevel <o.v.shevel@gmail.com>
2024-06-05 22:43:40 +0300

    env config created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,26 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cat } from './cats/cats.entity';
 import { CatsModule } from './cats/cats.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'cats_db',
-      entities: [Cat],
-      synchronize: true,
-    }), CatsModule
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('HOST'),
+        port: +configService.get('PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE'),
+        entities: [Cat],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
